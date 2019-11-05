e2b2a2d7a840bfd6a74d7d8822ddfa861bbc2061
Katie Wright <katie.wrght@gmail.com>
2019-11-05 16:11:55 +0100

    Add timestamp support

--- knex.js
+++ knex.js
@@ -2,18 +2,20 @@ const config = require('./knexfile');
 const knex = require('knex')(config[process.env.NODE_ENV || 'development']);
 const bookshelf = require('bookshelf')(knex);
 
-const Larp = bookshelf.model('Larp', {
-  tableName: 'larps',
+const Larp = bookshelf.model("Larp", {
+  hasTimestamps: true,
+  tableName: "larps",
   characters() {
-    return this.hasMany('Character');
-  },
+    return this.hasMany("Character");
+  }
 });
 
-const Character = bookshelf.model('Character', {
-  tableName: 'characters',
+const Character = bookshelf.model("Character", {
+  hasTimestamps: true,
+  tableName: "characters",
   larp() {
-    return this.belongsTo('Larp');
-  },
+    return this.belongsTo("Larp");
+  }
 });
 
 module.exports = { knex, Character, Larp };
