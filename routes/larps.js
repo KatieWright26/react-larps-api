c3235ce9e09dad0c12dd5c64234fc74a9b7ecb72
Katie Wright <katie.wrght@gmail.com>
2019-11-05 10:16:18 +0100

    add update and delete routes. Install linter

--- routes/larps.js
+++ routes/larps.js
@@ -1,19 +1,41 @@
-var express = require('express');
-var router = express.Router();
-var knex = require('../knex');
+const express = require('express');
 
-/* GET larps listing. */
-router.get('/', function(req, res, next) {
-  knex.select().table("larps")
-    .then((larps) => res.json({ larps }))
-    .catch((error) => res.json({ error }))
+const router = express.Router();
+const knex = require('../knex');
+
+// CREATE larps
+router.post('/', function(req, res, next) {
+  knex('larps')
+    .insert({ name: req.body.name })
+    .then(larp => res.json({ larp }))
+    .catch(error => res.json({ error }));
 });
 
-// POST larps
-router.post('/', function(req, res, next) {
-  knex('larps').insert({ name: req.body.name })
-    .then((larp) => res.json({ larp}))
-    .catch((error) => res.json({ error }))
+/* READ larps */
+router.get('/', function(req, res, next) {
+  knex
+    .select()
+    .table('larps')
+    .then(larps => res.json({ larps }))
+    .catch(error => res.json({ error }));
+});
+
+// UPDATE larps
+router.patch('/', function(req, res, next) {
+  knex('larps')
+    .where('id', req.body.id)
+    .update({ name: req.body.name })
+    .then(larp => res.json({ larp }))
+    .catch(error => res.json({ error }));
+});
+
+// DELETE larps
+router.delete('/', function(req, res, next) {
+  knex('larps')
+    .where('id', req.body.id)
+    .del()
+    .then(larp => res.json({ larp }))
+    .catch(error => res.json({ error }));
 });
 
 module.exports = router;
