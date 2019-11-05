74129164ee26c0cc083ea12bd927ee1ea38aa20f
Katie Wright <katie.wrght@gmail.com>
2019-11-05 16:04:04 +0100

    import character route to app.js

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 
-var larps = require('./routes/larps');
+const larps = require('./routes/larps');
+const characters = require('./routes/characters');
 
-var app = express();
+const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser())
+app.use(cookieParser());
 
 app.use('/api/v1/larps', larps);
+app.use('/api/v1/characters', characters);
 
 module.exports = app;
